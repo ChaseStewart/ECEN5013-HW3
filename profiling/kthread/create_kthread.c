bed694dd1a9f93473e46b4ee2e9ab748765a69d8
Chase E Stewart <chst9148@colorado.edu>
2017-10-03 20:35:06 -0600

    switch to use jiffies

--- profiling/kthread/create_kthread.c
+++ profiling/kthread/create_kthread.c
@@ -1,54 +1,29 @@
 #include <linux/kthread.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/time.h>
+#include <linux/jiffies.h>
 
-/*
- * struct kthread_create_info
- * {
- * 	/ Information passed to kthread() from kthreadd. /
- * 	int (*threadfn)(void *data);
- * 	void *data;
- * 	int node;
- * 
- * 	/ Result passed back to kthread_create() from kthreadd. /
- * 	struct task_struct *result;
- * 	struct completion *done;
- * 
- * 	struct list_head list;
- * };
- * 
- * struct kthread {
- * 	unsigned long flags;
- * 	unsigned int cpu;
- * 	void *data;
- * 	struct completion parked;
- * 	struct completion exited;
- * };
- * 
- */
+int kthread_function(void *data){
+	printk("[profile_kthreads][child] Hello from child\n");
+	do_exit(0);
+}
 
 int get_kthread_time(void){
-	clock_t kthread_start, kthread_end;
-	double kthread_time;
+	unsigned long kthread_start, kthread_end, kthread_time;
 	struct task_struct *kthread_task;
 
 	printk("[profile_kthreads] Initialize module\n");
-	kthread_start=clock();
-	if (kthread_start == (clock_t)-1)
-	{
-		/* failed to get CPU ticks */
-		printk("[profile_kthreads] Failed to get start time\n");
-	}
+	
+	kthread_start = get_jiffies_64();	
+	
 	/* call kthread_create */
-	kthread_end=clock();
-	if (kthread_start == (clock_t)-1)
-	{
-		/* failed to get CPU ticks */
-		printk("[profile_kthreads] Failed to get end time\n");
-	}
-	kthread_time = kthread_end - kthread_start;
-	printk("[profile_kthreads] kthread CPU time was %f\n", kthread_time);
+	kthread_task = kthread_create(kthread_function, NULL, NULL);
+	wake_up_process(kthread_task);
+
+	kthread_end = get_jiffies_64();	
+	
+	kthread_time = (kthread_end - kthread_start);
+	printk("[profile_kthreads] kthread CPU time was %ld\n", kthread_time);
 	return 0;
 }
 
